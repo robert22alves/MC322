0640d993b67a28c346fed3f8610dc885a8ca79a6
LGC250407 <141692253+LGC250407@users.noreply.github.com>
2023-08-11 13:30:50 -0300

    Delete Emp_livros.java

--- lab01/lab01/src/controle_livros/Emp_livros.java
+++
@@ -1,9 +0,0 @@
-package controle_livros;
-
-public class Emp_livros {
-    protected String membro;
-    protected String funcao; //Aluno ou Docente
-    protected String registro; //RA para Alunos e Numero de Matricula para Docentes
-    protected String data_emprestimo;
-    protected String data_devolucao;
-}
