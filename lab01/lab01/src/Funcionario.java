1c2026f855856cb6bb143e28a4b3e37a978c5c28
robert22alves <141692183+robert22alves@users.noreply.github.com>
2023-08-13 15:48:32 -0300

    Add files via upload

--- lab01/lab01/src/Funcionario.java
+++ lab01/lab01/src/Funcionario.java
@@ -1,5 +1,21 @@
 public class Funcionario {
     private String nome;
     private String cpf;
-    private String instituto;
+    private String funcao;
+
+    public Funcionario(String nome, String cpf, String funcao){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.funcao = funcao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public String getFuncao() {
+        return funcao;
+    }
 }
