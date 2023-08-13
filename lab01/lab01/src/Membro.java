f6e249646009b6041a51b6d1eb2101155067e669
LGC250407 <141692253+LGC250407@users.noreply.github.com>
2023-08-13 15:02:22 -0300

    Delete Membro.java

--- lab01/lab01/src/Membro.java
+++
@@ -1,9 +0,0 @@
-public class Membro {
-    private String nome;
-    private String funcao; //Aluno ou Docente
-    private String registro; //RA para Alunos e Numero de Matricula para Docentes
-    private String atividade; //Nenhuma, Emprestimo ou Suspensão
-    private boolean suspenso;
-    private int qtd_emprestimo;
-    //lista com nome dos livro,data de emprestimo, data de devolucao
-}
