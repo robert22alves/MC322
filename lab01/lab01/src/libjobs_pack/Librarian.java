803fc4009d7ca64efd7891547cdbdf95dc4f7fa8
LGC250407 <141692253+LGC250407@users.noreply.github.com>
2023-08-30 11:22:15 -0300

    Organizando o Repositório

--- lab01/lab01/src/libjobs_pack/Librarian.java
+++
@@ -1,9 +0,0 @@
-package libjobs_pack;
-
-import miscellaneous.Person;
-
-public class Librarian extends Person{
-    public Librarian(String firstname, String surname, int cpf) {
-        super(firstname, surname, cpf);
-    }
-}
--- lab01/lab01/src/multimedia_pack/Media.java
+++
@@ -1,7 +0,0 @@
-package multimedia_pack;
-
-public abstract class Media extends Item{
-    public Media(String title, float price, float fine) {
-        super(title, price, fine);
-    }
-}
