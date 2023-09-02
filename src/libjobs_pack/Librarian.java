c1ff379e1d89a2fe6bf272a1f3ea17967fc5234d
LGC250407 <l250407@dac.unicamp.br>
2023-09-02 18:01:00 -0300

    Ajustando os Funcionarios

--- src/libjobs_pack/Librarian.java
+++ src/libjobs_pack/Librarian.java
@@ -1,9 +1,93 @@
 package libjobs_pack;
 
+import libmembers_pack.Member;
 import libmembers_pack.Staff;
+import libservices_pack.Lending;
+import libservices_pack.Service;
+import multimedia_pack.Item;
+import system.Collection;
+import system.Membership;
 
 public class Librarian extends Staff{
-    public Librarian(String firstname, String surname, int cpf, int id) {
+    private Collection itens;
+    private Membership membership;
+
+    public Librarian(String firstname, String surname, int cpf, int id, Collection itens, Membership membership) {
         super(firstname, surname, cpf, id);
+        this.itens = itens;
+        this.membership = membership;
+    }
+
+    private Member searchMember(int typeMember, String member) {
+        Member m = null;
+        switch (typeMember) {
+            case 0:
+                m = membership.returnGrad(member);
+                break;
+        
+            case 1:
+                m = membership.returnPgrad(member);
+                break;
+            
+            case 2:
+                m = membership.returnProfessor(member);
+                break;
+            
+            case 3:
+                m = membership.returnStaff(member);
+                break;
+        }
+        return m;
+    }
+
+    private Item searchItem(int typeItem, String item) {
+        Item i = null;
+        switch (typeItem) {
+            case 0:
+                i = itens.returnBook(item);
+                break;
+        
+            case 1:
+                i = itens.returnEbook(item);
+                break;
+            
+            case 2:
+                i = itens.returnCD(item);
+                break;
+            
+            case 3:
+                i = itens.returnDVD(item);
+                break;
+        }
+        return i;
+    }
+
+    public void makeLending(int typeItem, String item, int typeMember, String member, String lendDate, String returnDate) {         
+        Item i = searchItem(typeItem, item);
+        Member m = searchMember(typeMember, member);
+        Service.makeLending(i, m, lendDate, returnDate);
+    }
+
+    public void makeReturn(int typeMember, String member, Lending lending, String returnDate, boolean isOverdue ,int overdueDays) {         
+        Member m = searchMember(typeMember, member);
+        Service.makeReturn(lending, m, returnDate, isOverdue, overdueDays);        
+    }
+
+    public void makeRenewal(Lending lending, String renewalDate, String returnDate) {         
+        Service.makeRenewal(lending, renewalDate, returnDate);
+    }
+
+    public void makeLen(int typeItem, String item, int typeMember, String member, String reserveDate) {         
+        Item i = searchItem(typeItem, item);
+        Member m = searchMember(typeMember, member);
+        Service.makeReserve(i, m, reserveDate);
+    }
+
+    public Collection getItens() {
+        return itens;
+    }
+
+    public Membership getMembership() {
+        return membership;
     }
 }
