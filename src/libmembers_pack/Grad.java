2e974f0e369fa9593b8194ed08e6f5481623b050
LGC250407 <141692253+LGC250407@users.noreply.github.com>
2023-09-10 13:30:49 -0300

    Organizando o Repositório

--- src/libmembers_pack/Grad.java
+++
@@ -1,7 +0,0 @@
-package libmembers_pack;
-
-public class Grad extends Student{
-    public Grad(String firstname, String surname, int cpf, int id, String institute, String course, String integration, String contact, String address, String registration_date) {
-        super(firstname, surname, cpf, id, 3, 15, 1, institute, course, integration, contact, address, registration_date);
-    }
-}
