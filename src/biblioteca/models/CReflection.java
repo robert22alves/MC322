b14a9f90dc7efaf7eed13b725190c9021629cf92
robert22alves <141692183+robert22alves@users.noreply.github.com>
2023-11-04 19:08:22 -0300

    Imprimir conteudo de lista

--- src/biblioteca/models/CReflection.java
+++ src/biblioteca/models/CReflection.java
@@ -4,6 +4,9 @@ import java.lang.reflect.*;
 
 import biblioteca.models.acervo.itensMultimidia.ItemMultimidia;
 import biblioteca.models.membros.Membro;
+import biblioteca.models.acervo.salas.Sala;
+import biblioteca.models.servicos.Emprestimo;
+
 
 public class CReflection {
     public static void imprimirAtributos(Object object) {
@@ -37,6 +40,26 @@ public class CReflection {
             Class<?> class1 = object.getClass();
             for (Method m : class1.getMethods()) {
                 System.out.println(m);
+            }
+
+        }
+        catch (IllegalArgumentException e) {
+            System.err.println("Parametro incorreto: " + e.getMessage());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void imprimirLista (Arraylist<?> lista){
+        try {
+
+            if (!(lista.get(0) instanceof ItemMultimidia || lista.get(0) instanceof Membro || lista.get(0) instanceof Sala || lista.get(0) instanceof Emprestimo)) {
+                throw new IllegalArgumentException("Membro, Item Multimidia, Sala ou Emprestimo");
+            }
+
+            Arraylist<?> array1 = lista.getField();
+            for (Field a : array1.getField()) {
+                System.out.println(a);
             }
 
         }
